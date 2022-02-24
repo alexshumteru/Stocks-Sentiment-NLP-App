12d3c3399771c712532753c113229f46382203f8
Alex Shum <alex.shum.sf@gmail.com>
2022-02-24 19:23:41 +0800

    Pushing frontend changes

--- StocksTweetsPredictor/StocksTweetsPredictor/ViewController.swift
+++ StocksTweetsPredictor/StocksTweetsPredictor/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTweet: UITextField!
+    
+    
+    @IBOutlet weak var tweetURL: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        result.text = ""
         // Do any additional setup after loading the view.
     }
 
-
+   
+    
 }
 
